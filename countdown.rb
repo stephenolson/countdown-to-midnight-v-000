16ba12c10002292f0710416d6174a0afb1c36aa2
Stephen Olson <stephenolson@gmail.com>
2018-09-19 19:12:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,9 +6,9 @@ def countdown
   
     loop do
       number -=1
-      if number.between?(10,2)
+      while number.between?(10,2)
         puts "#{number} SECONDS!"
-      elseif number == 1 
+      while number == 1 
         puts "#{number} SECOND!"
       else
         puts "HAPPY NEW YEAR!"
