ca0ab5d84010b9835499bc0a51299394b743d626
Stephen Olson <stephenolson@gmail.com>
2018-09-19 20:18:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,3 +12,12 @@ def countdown
 end
 
 
+def countdown_with_sleep
+  
+      while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        sleep(1)
+      end
+        puts "HAPPY NEW YEAR!"
+end
